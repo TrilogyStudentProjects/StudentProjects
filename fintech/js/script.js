9bab6f8c7934e58305e15bf7431c9fce88f18240
Gretel Martinez <23240307+grety22@users.noreply.github.com>
2021-07-09 11:47:16 -0400

    Fintech - Changes after design 2nd QA

--- fintech/js/script.js
+++ fintech/js/script.js
@@ -1,10 +1,4 @@
-document.body.style.overflow = 'hidden';
 $(document).ready(function(){
-    $('.modal').modal().on('shown', function(){
-        $('body').css('overflow', 'hidden');
-    }).on('hidden', function(){
-        $('body').css('overflow', 'auto');
-    })
     // FOOTER
     var d = new Date(), output = d.getFullYear();        
     $("#year").text(output);
